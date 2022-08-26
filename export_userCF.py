e30b180cd2abf7f632fde4cad50f9d4262173675
hong <873696121@qq.com>
2022-08-26 11:57:12 +0800

    Revert "temp save"
    This reverts commit 14b6751529fe5b0f5615c463df4972a1026b78a0.

--- export_userCF.py
+++ export_userCF.py
@@ -143,6 +143,9 @@ class UserBasedCF():
             rec_count += N
             test_count += len(test_movies)
 
+        res += "输入user: %d" % cnt
+        res += " rec user = %d" % len(res_user_dict)
+        res += " rec item = %d" % len(res_item_dict)
         precision = hit / (1.0 * rec_count)
         recall = hit / (1.0 * test_count)
         coverage = len(all_rec_movies) / (1.0 * self.movie_count)
